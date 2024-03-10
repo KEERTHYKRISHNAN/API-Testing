eaf192777df35a9291f8925721d94b9594febcb4
Keerthy Krishnan <keerthy310595@gmail.com>
2024-03-10 01:00:35 -0600

    Pushing Labs

---
+++ src/test/java/APITesting/Helloworldd.java
@@ -0,0 +1,7 @@
+package APITesting;
+
+public class Helloworldd {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!!");
+    }
+}
---
+++ src/test/java/APITesting/SimpleAPI.java
@@ -0,0 +1,21 @@
+package APITesting;
+
+import io.restassured.RestAssured;
+
+public class SimpleAPI {
+    public static void main(String[] args) {
+        //URL: https://api.zippopotam.us/IN/560037
+        //Base URL: https://api.zippopotam.us
+        //Base Path:/IN/560037
+        //Get Method
+        //No Payload
+        //No Authentication
+        //We can see log by adding.log().all()
+
+        //Validate STATUS CODE, RESPONSE BODY
+
+        RestAssured.given().baseUri("https://api.zippopotam.us").basePath("/IN/560037")
+                     .when().log().all().get()
+                     .then().log().all().statusCode(200);
+    }
+}
